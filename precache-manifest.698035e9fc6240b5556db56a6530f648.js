4c4e83c49f4b06dcfdfbd9608d12bc5432c6e42f
chartung <chartung@seas.upenn.edu>
2020-08-29 13:10:42 -0700

    Updates

---
+++ precache-manifest.698035e9fc6240b5556db56a6530f648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ad0026e42a9b3e37265a28fe193270",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "4c7f1b1d1ce507d7e75b",
+    "url": "/markdown-previewer/static/css/main.b4e84996.chunk.css"
+  },
+  {
+    "revision": "18fe1d955ec48017014d",
+    "url": "/markdown-previewer/static/js/2.37d8bce9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.37d8bce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7f1b1d1ce507d7e75b",
+    "url": "/markdown-previewer/static/js/main.7667e229.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
